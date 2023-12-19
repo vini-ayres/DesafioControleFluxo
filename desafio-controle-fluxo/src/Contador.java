a43884365926fbc0e1eeb185dd772a5a920735a4
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2023-12-18 23:39:06 -0300

    Mudança do exception

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Contador {
 	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
 		try {
+			Scanner terminal = new Scanner(System.in);
+			System.out.println("Digite o primeiro parametro");
+			int parametroUm = terminal.nextInt();
+			System.out.println("Digite o segundo parametro");
+			int parametroDois = terminal.nextInt();
+
+			try {
 			// chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		} catch (ParametrosInvalidosException exception) {
-			// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+			} catch (ParametrosInvalidosException exception) {
+				// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+				System.out.println("O segundo parametro deve ser maior que o primeiro");
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Os parametros precisam ser numeros inteiros");
 		}
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
